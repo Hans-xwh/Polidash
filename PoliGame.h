6015201edffb9df33f1914f4e1c3bfb4ad99dfbf
noeliann <noeliaconsultas@gmail.com>
2025-11-08 23:56:33 -0500

    Music

--- PoliGame.h
+++ PoliGame.h
@@ -59,8 +59,9 @@ namespace Polidash {
 		//Jugador* jugador;
 		Juego* juego;
 	private: System::Windows::Forms::ProgressBar^ progressBar1;
-	private: System::Windows::Forms::Button^ button1;
-	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Label^ label1;
+
+
 
 
 
@@ -84,8 +85,7 @@ namespace Polidash {
 			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(PoliGame::typeid));
 			this->timer1 = (gcnew System::Windows::Forms::Timer(this->components));
 			this->progressBar1 = (gcnew System::Windows::Forms::ProgressBar());
-			this->button1 = (gcnew System::Windows::Forms::Button());
-			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->SuspendLayout();
 			// 
 			// timer1
@@ -103,33 +103,21 @@ namespace Polidash {
 			this->progressBar1->TabIndex = 0;
 			this->progressBar1->Click += gcnew System::EventHandler(this, &PoliGame::progressBar1_Click);
 			// 
-			// button1
+			// label1
 			// 
-			this->button1->Location = System::Drawing::Point(22, 692);
-			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(112, 23);
-			this->button1->TabIndex = 1;
-			this->button1->Text = L"Play Music";
-			this->button1->UseVisualStyleBackColor = true;
-			this->button1->Click += gcnew System::EventHandler(this, &PoliGame::button1_Click);
-			// 
-			// button2
-			// 
-			this->button2->Location = System::Drawing::Point(160, 692);
-			this->button2->Name = L"button2";
-			this->button2->Size = System::Drawing::Size(112, 23);
-			this->button2->TabIndex = 2;
-			this->button2->Text = L"Stop Music";
-			this->button2->UseVisualStyleBackColor = true;
-			this->button2->Click += gcnew System::EventHandler(this, &PoliGame::button2_Click);
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(12, 699);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(308, 16);
+			this->label1->TabIndex = 1;
+			this->label1->Text = L"Pulsa Z para activar y C para desactivar la música.";
 			// 
 			// PoliGame
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(1703, 730);
-			this->Controls->Add(this->button2);
-			this->Controls->Add(this->button1);
+			this->Controls->Add(this->label1);
 			this->Controls->Add(this->progressBar1);
 			this->Icon = (cli::safe_cast<System::Drawing::Icon^>(resources->GetObject(L"$this.Icon")));
 			this->KeyPreview = true;
@@ -141,6 +129,7 @@ namespace Polidash {
 			this->KeyDown += gcnew System::Windows::Forms::KeyEventHandler(this, &PoliGame::PoliGame_KeyDown);
 			this->KeyUp += gcnew System::Windows::Forms::KeyEventHandler(this, &PoliGame::PoliGame_KeyUp);
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
@@ -190,6 +179,21 @@ namespace Polidash {
 		if (e->KeyCode == Keys::Left)	teclapulsada = Dir::IZQUIERDA;
 
 		juego->KeyDown(teclapulsada);
+
+		static bool isMusicActiva = false;
+
+		if (e->KeyCode == Keys::Z) {
+			if (!isMusicActiva) {
+				soniditoTime->PlayLooping();
+				isMusicActiva = true;
+			}
+		}
+		if (e->KeyCode == Keys::C) {
+			if (isMusicActiva) {
+				soniditoTime->Stop();
+				isMusicActiva = false;
+			}
+		}
 	}
 
 	private: System::Void PoliGame_KeyUp(System::Object^ sender, KeyEventArgs^ e) {
@@ -200,12 +204,7 @@ namespace Polidash {
 	}
 private: System::Void progressBar1_Click(System::Object^ sender, System::EventArgs^ e) {
 }
-private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
-	soniditoTime->PlayLooping();
-}
-private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
-	soniditoTime->Stop();
-}
+
 };
 }
 
